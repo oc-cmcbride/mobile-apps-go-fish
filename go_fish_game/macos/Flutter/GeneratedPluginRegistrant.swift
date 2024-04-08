73a7487c138e691c19dd13f06bd9d349974737b5
ari.t <arriannaomi.a.taton@eagles.oc.edu>
2024-04-07 20:41:26 -0500

    added the google-services.json file and resolved dependencies

--- go_fish_game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ go_fish_game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_auth
 import firebase_core
 import google_sign_in_ios
 import path_provider_foundation
@@ -13,6 +14,7 @@ import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
