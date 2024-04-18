b0d8b8eca1dd16abdac265be8cec05a2a08d2885
ari.t <arriannaomi.a.taton@eagles.oc.edu>
2024-04-18 10:09:14 -0500

    changed the name of app and game icon

--- go_fish_game/lib/main.dart
+++ go_fish_game/lib/main.dart
@@ -79,7 +79,7 @@ class MyApp extends StatelessWidget {
           final palette = context.watch<Palette>();
 
           return MaterialApp.router(
-            title: 'My Flutter Game',
+            title: 'Go Fish',
             theme: ThemeData.from(
               colorScheme: ColorScheme.fromSeed(
                 seedColor: palette.darkPen,
